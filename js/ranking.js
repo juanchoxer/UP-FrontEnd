247e0ab8bde3e325a5fee8fde2e0fb3dd8e58dbc
Juan Mincucci <c.jmincucci@shipbob.com>
2024-06-09 13:23:00 -0500

    Agrego contador al ranking

--- js/ranking.js
+++ js/ranking.js
@@ -3,25 +3,34 @@ window.onload = async function () {
 
     fetch(API_URL + '/ranking')
     .then(response => response.json())
-    .then(json => mostrarRepos(json))
+    .then(json => mostrarRanking(json))
     .catch(err => console.log('Solicitud fallida', err));
 };
 
-function mostrarRepos(json){
+function mostrarRanking(json){
     let repolist = document.getElementById("rankingLista");
     json.forEach((element,index) => {
 
         let li = document.createElement("li");
-        li.setAttribute("id",index+1);
+        li.setAttribute("id","lineaRanking");
 
         div = document.createElement("div");
-
-        li.appendChild(div);
-
+       
         let img = document.createElement("img");
         img.src = "images\\" + element.modelo.toLowerCase() + ".jpg";
         img.classList.add("imagenesRanking")
         div.appendChild(img);
+
+        li.appendChild(div);
+
+        divTexto = document.createElement("div");
+        
+        divTexto.textContent = "pedido " + element.contadorPedidos + " veces"; 
+        divTexto.style.fontSize = '180%';
+        divTexto.style.paddingLeft = '10px';  
+        divTexto.style.marginBottom = '35px';  
+
+        li.appendChild(divTexto);
 
         repolist.appendChild(li);
     });
